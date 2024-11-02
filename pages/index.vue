aadaf05c1b2334e25105c0523a007d2f6a6f6f40
Mehr-code <papa.molla8@gmail.com>
2024-11-02 21:29:27 +0330

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="w-screen h-screen bg-[#333] p-12">
-    <nav class="">
-      <h2 class="">رستوران پیربابا</h2>
-    </nav>
+  <!-- Main page -->
+  <div class="w-screen h-screen py-15 p-x10 flex justify-center items-center">
+    <!-- Yellow box -->
+    <div
+      class="w-[80%] h-[80%] border-8 border-[#ffb80e] flex flex-row relative bg-[#982121] rounded-3xl"
+    >
+      <!-- Logo -->
+      <div class="absolute top-6 left-12 flex flex-row gap-3 items-center">
+        <img src="../UI/logo.png" alt="Pir BaBa's logo" class="w-20 h-20" />
+        <p class="font-bold text-5xl text-[#ffb80e]">Pir BaBa</p>
+      </div>
+
+      <!-- Hamburger PIC -->
+      <div class="w-[40%] flex justify-center items-center">
+        <img src="../UI/pic1.png" alt="Hamburger PIC" class="w-[90%] mt-32" />
+      </div>
+
+      <!-- Login -->
+      <div class="w-[60%]"></div>
+    </div>
   </div>
 </template>
+
+<script setup></script>
+
+<style scoped></style>
